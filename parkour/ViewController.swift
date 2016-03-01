b3f706792e5a13a357e6ea32b82b2b262cd5a448
Shu Iida <shuiida@me.com>
2016-03-01 12:57:15 +0900

    Initial Commit

---
+++ parkour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  parkour
+//
+//  Created by Shu Iida on 2016/03/01.
+//  Copyright © 2016年 Shu Iida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
